faa2a84f9b3cbf5b5575120674aedc6abd655b0e
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-13 13:28:14 +0300

    Master
    Change data

--- src/components/OrderItem/OrderItem.jsx
+++ src/components/OrderItem/OrderItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import s from './OrderItem.module.css'
 import GameCover from '../GameCover/GameCover';
+import { useDispatch } from 'react-redux';
+import { AiOutlineCloseCircle} from 'react-icons/ai'
+import { deleteItemFromCart } from '../../redux/cart/reducer';
 
 function OrderItem({game}) {
+  const dispatch = useDispatch()
 
+  const handleClick = () => {
+    dispatch(deleteItemFromCart(game.id))
+  }
 
   return (
     <div className={s.orderItem}>
@@ -15,6 +22,10 @@ function OrderItem({game}) {
       </div>
       <div className={s.price}>
         <span>{game.price} ₴</span>
+        <AiOutlineCloseCircle 
+          size={25}
+          className={s.deleteIcon}
+          onClick={handleClick}/>
       </div>
     </div>
   )
